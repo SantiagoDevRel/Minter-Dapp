71cf550e7e73fc0c18b96c9db1c173b71a878c03
Santiago Trujillo Zuluaga <stztrujillo@hotmail.com>
2023-02-28 18:28:36 +0000

    Updated footer with new Github link

--- src/components/General_Components/Footer.js
+++ src/components/General_Components/Footer.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 import { SiGithub } from "react-icons/si";
 
-
 function Footer() {
-    return (
-        <div>
-            <footer><a href='https://github.com/strujilloz/Metana/tree/main/Week3/erc1155-multitoken'><SiGithub /> Santiago Trujillo Zuluaga <SiGithub /> </a></footer>
-        </div>
-    )
+  return (
+    <div>
+      <footer>
+        <a href="https://github.com/santiagotrujilloz/Minter-Dapp">
+          <SiGithub /> Santiago Trujillo Zuluaga <SiGithub />{" "}
+        </a>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
